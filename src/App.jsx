5be6466ff649602c31384697b8aea307525046ed
PopescuTudor <94108303+PopescuTudor@users.noreply.github.com>
2023-10-17 20:05:14 +0000

    add details component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './normalize.css';
 import Layout from './components/layout';
+import Details from './components/Details';
 import styled from 'styled-components';
 export default function App() {
   return (
     <Layout 
       left = {
-        <div>left side</div>
+        <div>
+          <Details />
+        </div>
       }
       right = {
         <div>right side</div>
