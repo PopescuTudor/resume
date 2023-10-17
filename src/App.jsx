c5c5a561fb3e1e712cd80321badb691aefb276c7
PopescuTudor <94108303+PopescuTudor@users.noreply.github.com>
2023-10-17 18:49:59 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import styled from 'styled-components';
+export default function App() {
+  return (
+    <Main>
+      React ⚛️ + Vite ⚡ + Replit 🌀
+    </Main>
+  )
+}
+
+const Main = styled.main`
+    color: red;
+  `
+
