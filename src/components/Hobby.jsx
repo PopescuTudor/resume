182aa31662e05d1e9e46af6aa7fb81b2aae77790
PopescuTudor <94108303+PopescuTudor@users.noreply.github.com>
2023-10-20 11:35:19 +0000

    Added hobbies component

--- src/components/Hobby.jsx
+++ src/components/Hobby.jsx
@@ -2,9 +2,34 @@ import styled from 'styled-components';
 
 const Hobby = () => {
   return (
-    <div></div>
+    <div>
+      <Title>Hobbies</Title>
+      <Hobbies>Tennis./.. Hiking./.. Skiing./.. Technology./.. Automotive</Hobbies>
+    </div>
     
   );
 };
+
+const Title = styled.div`
+  color: #000;
+  font-family: Lato;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 800;
+  line-height: 100%; /* 18px */
+  margin-top: 30px;
+  margin-left: 30px;
+`;
+
+const Hobbies = styled.div`
+  color: #000;
+  font-family: Lato;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 800;
+  line-height: 100%;
+  margin-top: 15px;
+  margin-left: 30px;
+`;
 
 export default Hobby;
